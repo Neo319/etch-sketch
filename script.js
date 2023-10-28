c5cd4da571897a378863b52ed49ae1bd57d94369
Neo319 <alexmnevins@gmail.com>
2023-10-28 13:11:06 -0400

    update styles, remove temporary text

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const grid = document.getElementById ('grid');
 for (i = 0; i <= 15; i++) {
     let column = document.createElement("div");
     column.className = `column ${i}`;
-    column.textContent = `column ${i}`;
+    // column.textContent = `column ${i}`;
 
     for (j = 0; j <= 15; j++) {
         let row = document.createElement("div");
         row.className = `row ${j}`;
-        row.textContent = `row ${j}`;
+        // row.textContent = `a`;
 
         column.appendChild(row);
 
