44fa2a089f9dc9f5d06e4f0ff690dc73fb4b8972
Neo319 <alexmnevins@gmail.com>
2023-10-28 11:16:10 -0400

    create html for grid

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log ("Hello world!");
+
+const grid = document.getElementById ('grid');
+
+for (i = 0; i <= 15; i++) {
+    let column = document.createElement("div");
+    column.className = `column ${i}`;
+    column.textContent = `column ${i}`;
+
+    for (j = 0; j <= 15; j++) {
+        let row = document.createElement("div");
+        row.className = `row ${j}`;
+        row.textContent = `row ${j}`;
+        column.appendChild(row);
+
+
+    }
+    
+    grid.appendChild(column);
+}
